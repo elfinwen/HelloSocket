b3f5e59576a09ff9aa9ad821c06a4ff0b00f89f6
elfinwen <23276424@qq.com>
2018-04-08 13:57:59 +0800

    客户端跨平台移植 支持WINDOWS LINUX MACOS 1.4

--- EasyTcpClient/client.cpp
+++ EasyTcpClient/client.cpp
@@ -1,6 +1,18 @@
 #define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include <WinSock2.h>
+
+#ifdef _WIN32
+	#include <windows.h>
+	#include <WinSock2.h>
+#else
+	#include <unistd.h> //uni std
+	#include <arpa/inet.h>
+	#include <string.h>
+
+	#define SOCKET int
+	#define INVALID_SOCKET  (SOCKET)(~0)
+	#define SOCKET_ERROR            (-1)
+#endif
+
 #include <stdio.h>
 #include <thread>
 
@@ -79,45 +91,44 @@ int processor(SOCKET _cSock)
 	//缓冲区
 	char szRecv[1024] = {};
 	//5 接收客户端的数据
-	int nLen = recv(_cSock, szRecv, sizeof(DataHeader), 0);
+	int nLen = (int)recv(_cSock, szRecv, sizeof(DataHeader), 0);
 	DataHeader* header = (DataHeader*)szRecv;
 	if (nLen <= 0)
 	{
-		printf("与服务器断开连接，任务结束。\n", _cSock);
+		printf("与服务器断开连接，任务结束。\n");
 		return -1;
 	}
 	//6 处理请求
 	switch (header->cmd)
 	{
-	case CMD_LOGIN_RESULT:
-	{
-		recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
-		LoginResult* loginresult = (LoginResult*)szRecv;
-		printf("收到服务端消息：CMD_LOGIN_RESULT,数据长度：%d\n", header->dataLength);
-	}
-	break;
-	case CMD_LOGOUT_RESULT:
-	{
-		recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
-		LogoutResult* logoutresult = (LogoutResult*)szRecv;
-		printf("收到服务端消息：CMD_LOGOUT_RESULT,数据长度：%d\n", header->dataLength);
-	}
-	break;
-	case CMD_NEW_USER_JOIN:
-	{
-		recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
-		NewUserJoin* newuserjoin = (NewUserJoin*)szRecv;
-		printf("收到服务端消息：CMD_NEW_USER_JOIN,数据长度：%d\n", _cSock, header->dataLength);
+		case CMD_LOGIN_RESULT:
+		{
+			recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
+			LoginResult* loginresult = (LoginResult*)szRecv;
+			printf("收到服务端消息：CMD_LOGIN_RESULT,数据长度：%d\n", header->dataLength);
+		}
+		break;
+		case CMD_LOGOUT_RESULT:
+		{
+			recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
+			LogoutResult* logoutresult = (LogoutResult*)szRecv;
+			printf("收到服务端消息：CMD_LOGOUT_RESULT,数据长度：%d\n", header->dataLength);
+		}
+		break;
+		case CMD_NEW_USER_JOIN:
+		{
+			recv(_cSock, szRecv + sizeof(DataHeader), header->dataLength - sizeof(DataHeader), 0);
+			NewUserJoin* newuserjoin = (NewUserJoin*)szRecv;
+			printf("收到服务端消息：CMD_NEW_USER_JOIN,数据长度：%d\n", _cSock, header->dataLength);
 
-	}
-	break;
-
-	default:
-	{
-		DataHeader _header = { 0, CMD_ERROR };
-		send(_cSock, (char*)&_header, sizeof(DataHeader), 0);
-	}
-	break;
+		}
+		break;
+		//default:
+		//{
+		//	DataHeader _header = { 0, CMD_ERROR };
+		//	send(_cSock, (char*)&_header, sizeof(DataHeader), 0);
+		//}
+		//break;
 	}
 
 	return 0;
@@ -158,10 +169,12 @@ void cmdThread(SOCKET sock)
 
 int main()
 {
+#ifdef _WIN32
 	//启动Windows socket 2.x环境
 	WORD ver = MAKEWORD(2, 2);//版本号，对应上面的头文件Winsock2.h  
 	WSADATA dat;
 	WSAStartup(ver, &dat);//与WSACleanup()是一一对应关系
+#endif
 	//----------------以下是socket网络通信部分
 	//-- 用Socket API建立简易TCP客户端
 	// 1 建立一个socket
@@ -178,7 +191,11 @@ int main()
 	sockaddr_in _sin = {};
 	_sin.sin_family = AF_INET;
 	_sin.sin_port = htons(4567);//服务器定义的端口号
+#ifdef _WIN32
 	_sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");//inet_addr("127.0.0.1")--本机测试用;inet_addr("192.168.0.103");
+#else
+	_sin.sin_addr.s_addr = inet_addr("192.168.16.1");
+#endif
 	int ret = connect(_sock, (sockaddr*)&_sin, sizeof(sockaddr_in));
 	if (SOCKET_ERROR == ret)
 	{
@@ -198,7 +215,7 @@ int main()
 		FD_ZERO(&fdReads);
 		FD_SET(_sock, &fdReads);
 		timeval t = {1,0}/*{0,0}*/;
-		int ret = select(_sock, &fdReads, 0, 0, &t);
+		int ret = select(_sock+1, &fdReads, 0, 0, &t);
 		if (ret < 0)
 		{
 			printf("select任务结束1\n");
@@ -215,15 +232,16 @@ int main()
 			}
 		}
 	}
-	
+#ifdef _WIN32
 	// 7 关闭套接字closesocket
 	closesocket(_sock);
-
 	//----------------以上是socket网络通信部分
 	//清除Windows socket环境
 	WSACleanup();//与WSAStartup(...)是一一对应关系
+#else
+	close(_sock);
+#endif
 	printf("已退出\n");
 	getchar();
-
 	return 0;
 }
